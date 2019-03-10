47dcbc59bbdf6767072cfc61dfe8c83b1faaf581
Pavel Zet <zetorama@gmail.com>
2019-03-09 16:56:03 -0800

    chore(web): add babel-runtime plugin to support async/await

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,9 @@ module.exports = {
                     '@babel/preset-env',
                     '@babel/preset-react',
                   ],
+                  plugins: [
+                    ['@babel/plugin-transform-runtime', { regenerator: true } ],
+                  ],
                 }
             }
         }, {
