ae7f8131de83d42c8be35757ae3b401bfb333fa0
Pavel Zet <zetorama@gmail.com>
2019-03-09 17:28:08 -0800

    fix(web): fix a typo in `welcome` cmp

--- src/client-web/components/welcome.jsx
+++ src/client-web/components/welcome.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Note from './note'
 
 export const Welcome = () => (
-  <Note ctx='light' title='Where is your next stop over?'>
+  <Note ctx='light' title='Where is your next stopover?'>
     <p>
       Don't sleep on the airport's floor anymore.<br />
       Get the cheapest room nearby and sleep there on any surface you like!
