54f8b8dfccc1407d0cf8eeb4ad1fdea7beaa7e02
Pavel Zet <zetorama@gmail.com>
2019-03-11 04:43:52 -0700

    fix(server): improve server - serve static and handle errors

--- src/server/app.js
+++ src/server/app.js
@@ -3,14 +3,19 @@ const morgan = require('morgan')
 const { api } = require('./api')
 
 const {
+  SERVER_SERVE_STATIC,
   SERVER_LOG_FORMAT = 'short',
-  NODE_ENV = 'development',
 } = process.env
 
 const app = exports.app = express()
 app.use(morgan(SERVER_LOG_FORMAT))
 
 app.use('/api/v1', api)
-app.use('*', (_req, res) => res.status(404).json({ error: 'Not Found' }))
+
+if (SERVER_SERVE_STATIC) {
+  app.use('/', express.static(SERVER_SERVE_STATIC))
+}
+app.use('*', (_req, res) => res.status(404).json({ error: 'Document Not Found' }))
+app.use((err, _req, res, _next) => res.status(err.status || 500).json({ error: err.message }))
 
 exports.default = app
