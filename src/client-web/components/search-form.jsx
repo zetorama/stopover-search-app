d3dd94a41e2bf25d6ff50a1dc14a57c93540d588
Pavel Zet <zetorama@gmail.com>
2019-03-10 22:28:37 -0700

    fix(web): prevent double-search for the same filter

--- src/client-web/components/search-form.jsx
+++ src/client-web/components/search-form.jsx
@@ -1,8 +1,12 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useState } from 'react'
 
 const NOOP = () => { }
 const localTS = () => Date.now() - new Date().getTimezoneOffset() * 60e3
 const tomorrow = () => new Date(localTS() + 24 * 3600e3).toISOString().split('T')[0]
+const isSimilar = (a, b) => a === b || a && b
+  && Object.keys(a).length === Object.keys(b).length
+  && Object.keys(a).every(key => a[key] === b[key])
+
 let id = Math.random()
 
 export const SearchForm = ({
@@ -13,11 +17,18 @@ export const SearchForm = ({
   onSubmit = NOOP,
   htmlId = ++id,
 }) => {
+  const data = { cityCode, checkInDate, checkOutDate }
+  const [submitted, setSubmitted] = useState({})
+  const isDisabled = isSimilar(submitted, data)
+
   const onCityCodeChange = useCallback(ev => onChange(ev, 'cityCode', ev.target.value), [onChange])
   const onCheckInDateChange = useCallback(ev => onChange(ev, 'checkInDate', ev.target.value), [onChange])
   const onCheckOutDateChange = useCallback(ev => onChange(ev, 'checkOutDate', ev.target.value), [onChange])
   const onFormSubmit = useCallback(
-    ev => onSubmit(ev, { cityCode, checkInDate, checkOutDate }),
+    (ev) => {
+      setSubmitted(data)
+      onSubmit(ev, data)
+    },
     [onSubmit, cityCode, checkInDate, checkOutDate],
   )
 
@@ -82,7 +93,13 @@ export const SearchForm = ({
         </div>
       </div>
       <div className='col-md-2 mb-3'>
-        <button type='submit' className='btn btn-success btn-block'>Search</button>
+        <button
+          disabled={isDisabled}
+          type='submit'
+          className='btn btn-success btn-block'
+        >
+          Search
+        </button>
       </div>
     </form>
   )
